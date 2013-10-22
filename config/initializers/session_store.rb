53312e81abf107cd0627a0321031620c3049f79b
Toby Downton <tobydownton@hotmail.com>
2013-10-22 13:53:54 +0100

    Initial Commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Extrabirthdays::Application.config.session_store :cookie_store, key: '_extrabirthdays_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Extrabirthdays::Application.config.session_store :active_record_store
