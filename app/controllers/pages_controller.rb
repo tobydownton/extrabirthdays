6c8acb6792b6910aa56d0794d2a5e259fb8ca754
Toby Downton <tobydownton@hotmail.com>
2013-10-27 13:23:28 +0000

    Added Calculate_Age action to Pages_Controller; and deleted public/index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def calculate_age
+  	# Extract date of birth
+  	# Assign it to an instance variablr
+  	# Render it on the page
+
+		@date_of_birth = params["post"]["date_of_birth"]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Extrabirthdays::Application.routes.draw do
-  get "pages/home"
+  root :to => 'pages#home'
+  post '/calculate_age', to: 'pages#calculate_age'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
