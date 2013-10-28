37dba85b9ef1fee7cea26403bac18872c1072c1e
Toby Downton <tobydownton@hotmail.com>
2013-10-28 13:31:09 +0000

    Add more date calculations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,22 @@ class PagesController < ApplicationController
 
   def calculate_age
   	# Extract date of birth
-  	# Assign it to an instance variablr
-  	# Render it on the page
-
+  	# Assign it to an instance variable
 		@date_of_birth = params["post"]["date_of_birth"]
+		@dob = @date_of_birth.to_time
+		@days_old = ((Time.now - @dob) / 1.day).round
+
+		# Find when last birthday was, amended from 
+		# http://stackoverflow.com/questions/12926126/calculate-days-until-next-birthday-in-rails
+		@bday = Date.new(Date.today.year, @dob.month, @dob.day)
+    @bday -= 1.year if Date.today <= @bday
+    (@bday - Date.today).to_i
+
+    #Find how many years old
+    @years = ((Time.now - @dob.to_time) / 1.year).round
+
+    #Find how many days since last birthday
+    @days_since_last_bday = ((Time.now - @bday.to_time) / 1.day).round
   end
 
 end
