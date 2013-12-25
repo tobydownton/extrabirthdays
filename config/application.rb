471b26da64e2ce04b2f81bd907f6f6c6752b1eee
Toby Downton <tobydownton@hotmail.com>
2013-12-25 18:26:03 +0000

    Add config.assets.initialize_on_precompile = false code to application.rb to try to fix heroku push problem

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Extrabirthdays
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Hoping this will fix the failed pushes to Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
